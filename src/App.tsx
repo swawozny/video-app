f3ef140e8992fa243dde6bd3495a0034459cf7a6
swawozny <56351762+swawozny@users.noreply.github.com>
2022-02-21 11:31:32 +0100

    Add new video input
    Closes #18
    
    - Add search bar
    - Add text input and button to confirm
    - Add app title and sections with description

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import {Route, Routes} from "react-router-dom";
 
 import TopBar from "./modules/TopBar/TopBar";
+import Home from "./pages/Home/Home";
+import pages from "./pages/pages";
 
 import "./App.css";
 
 function App() {
     return (
-        <TopBar/>
+        <>
+            <TopBar/>
+            <Home/>
+            <Routes>
+                {pages.map((page, index) => {
+                    return (
+                        <Route
+                            path={page.path}
+                            element={page.component}
+                            key={"page_" + index}
+                        >
+                        </Route>
+                    );
+                })}
+            </Routes>
+        </>
     );
 }
 
