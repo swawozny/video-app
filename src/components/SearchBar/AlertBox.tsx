29f020977accb2972ee39d5ddcdff51adcc981ff
swawozny <56351762+swawozny@users.noreply.github.com>
2022-02-26 16:09:01 +0100

    Add video adding interface
    Closes #17
    
    - Add videoServices array that contains objects with title and expression that must match user input url
    - Add VideoService with static method that add video to localStorage and method that sign videoService id to new video

---
+++ src/components/SearchBar/AlertBox.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {Alert, Col} from "reactstrap";
+
+type Props = {
+    videoAdded: boolean | null;
+};
+
+const AlertBox: React.FC<Props> = ({videoAdded}) => {
+    const getAlertColor = () => {
+        if (videoAdded) {
+            return "success";
+        }
+        return "danger";
+
+    };
+
+    const getAlertText = () => {
+        if (videoAdded) {
+            return "The video has been successfully added to the saved.";
+        }
+        return "The link provided is not valid!";
+
+    };
+
+    if (videoAdded === null) {
+        return null;
+    }
+
+    return (
+        <Col>
+            <Alert color={getAlertColor()}>
+                {getAlertText()}
+            </Alert>
+        </Col>
+    );
+};
+
+export default AlertBox;
--- src/components/SearchBar/InputBox.tsx
+++ src/components/SearchBar/InputBox.tsx
@@ -1,18 +1,57 @@
-import React from "react";
-import {Button, Col, Input, InputGroup} from "reactstrap";
+import React, {useState} from "react";
+import {Button, Col, Input, InputGroup, Spinner} from "reactstrap";
 
 type Props = {
     buttonText: string;
     inputPlaceHolder: string;
+    searchInput: string;
+    setSearchInput: (input: string) => void;
+    checkVideo: (url: string) => void;
 };
 
-const InputBox: React.FC<Props> = ({buttonText, inputPlaceHolder}) => {
+const InputBox: React.FC<Props> = ({buttonText, inputPlaceHolder, searchInput, setSearchInput, checkVideo}) => {
+    const [buttonClicked, setButtonClicked] = useState(false);
+
+    const getButtonText = () => {
+        if (buttonClicked) {
+            return "Processing...";
+        }
+        return buttonText;
+    };
+
+    const getButtonSpinner = () => {
+        return buttonClicked && <Spinner size="sm"/>;
+    };
+
+    const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newInput: string = event.target.value || "";
+        if (newInput) {
+            setSearchInput(newInput);
+        }
+    };
+
+    const handleClickButton = () => {
+        setButtonClicked(true);
+        checkVideo(searchInput);
+        setButtonClicked(false);
+    };
+
     return (
         <Col>
             <InputGroup size="lg">
-                <Input placeholder={inputPlaceHolder}/>
-                <Button color="primary">
-                    {buttonText}
+                <Input
+                    placeholder={inputPlaceHolder}
+                    onChange={handleChangeInput}
+                    value={searchInput}
+                    disabled={buttonClicked}
+                />
+                <Button
+                    color="primary"
+                    onClick={() => handleClickButton()}
+                    disabled={buttonClicked}
+                >
+                    {getButtonText()}
+                    {getButtonSpinner()}
                 </Button>
             </InputGroup>
         </Col>
